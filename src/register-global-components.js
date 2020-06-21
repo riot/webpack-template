941bd5b5fbb7977b0acd7fe8801391f4c1114599
Gianluca Guarini <gianluca.guarini@gmail.com>
2020-06-21 20:31:28 +0200

    Update register-global-components.js

--- src/register-global-components.js
+++ src/register-global-components.js
@@ -1,7 +1,7 @@
 import { register } from 'riot'
 import { basename } from 'path'
 
-const globalComponentsContext = require.context('./components/global/', true, /[a-zA-Z0-9-]+\.riot/);
+const globalComponentsContext = require.context('./components/global/', true, /[a-zA-Z0-9-]+\.riot/)
 
 export default () => {
   globalComponentsContext.keys().map(path => {
